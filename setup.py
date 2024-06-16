4fd3908a59bf2f0b6c2a0b50c7a7307411a7d345
Thecats <Thecats_Jfm@outlook.com>
2024-06-16 11:24:50 +0800

    update README.md and README_English.md and upgrede requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_packages
-
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 setup(
     name="14mv_draft",
     version="0.5",
     packages=find_packages(),
-    install_requires=[
-        'PyQt5==5.12',
-    ],
+    install_requires=required,
     entry_points={
         'console_scripts': [
             '14mvd=mv_draft.__main__:main', # 假设main函数在your_package/main.py中
